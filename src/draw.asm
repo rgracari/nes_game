9527e6945658cee723a6505ce076ead709600ad6
Raphael Ribeiro <raphael.ribeiro.pro@gmail.com>
2021-03-18 12:10:25 +0100

    Make all the player tiles moving accordingly.

--- src/draw.asm
+++ src/draw.asm
@@ -1,12 +1,18 @@
 DrawPlayer:
   ; test if the draw is called
-  LDA #$FB
-  STA test2
 
   LDA player1x
   STA $0200
+  STA $0200+4
+  ADC #$07
+  STA $0200+8
+  STA $0200+12
 
   LDA player1y
   STA $0203
+  STA $0203+8
+  ADC #$07
+  STA $0203+4
+  STA $0203+12
 
   RTS
