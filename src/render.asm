0fc5ba57a62b65f2aa4cf86b25ce5266313b24e3
Raphael Ribeiro <raphael.ribeiro.pro@gmail.com>
2021-03-18 11:34:07 +0100

    Adding OAM constants.

--- src/render.asm
+++ src/render.asm
@@ -2,9 +2,9 @@
 ;;;; transfer from CPU memory $0200 to PPU sprite memory
 TransferSpriteDMA:
   LDA #$00      ; setting the low byte $00 first
-  STA $2003     
+  STA OAMADDR     
   LDA #$02      ; the hight byte 
-  STA $4014     ; and the transfer will automaticly start
+  STA OAMDMA     ; and the transfer will automaticly start
                 
 ;;;; Clean up the PPU and be ready for the next interrupt
   LDA #%10010000    ; activate the NMI
